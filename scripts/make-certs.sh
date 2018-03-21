ee1c749fd8428d69a82800ffa1e0ca87c320c018
Hector del Rio <hector.del.rio@icloud.com>
2018-03-21 22:09:29 +0200

    - make-certs.sh script deletes unneeded .csr file - package.json scripts simplified

--- scripts/make-certs.sh
+++ scripts/make-certs.sh
@@ -15,5 +15,8 @@ openssl req -newkey rsa:4096 -keyout /certs/client_key.pem -out /certs/client_cs
 # Sign Client Certificate with our Server Certificate
 openssl x509 -req -in /certs/client_csr.pem -CA /certs/server_cert.pem -CAkey /certs/server_key.pem -out /certs/client_cert.pem -set_serial 01 -days 3650
 
+# Remove unneeded Certificate Sign Request
+rm /certs/client_csr.pem
+
 # Clean-up
 apk del openssl
